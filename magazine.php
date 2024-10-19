8a0e6ff414eac4c18d23e60d17e5241984e7c08a
shi-km <julietwanjiru333@gmail.com>
2024-10-19 12:02:35 +0300

    added a php file

---
+++ magazine.php
@@ -0,0 +1,66 @@
+
+
+<?php
+include("header.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timazi Africa</title>
+   <!-- cdn boostrap link -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+   crossorigin="anonymous">
+   <!-- custom css link -->
+   <link rel="stylesheet" href="styles.css" type="text/css">
+   <!-- favicon link -->
+   <link rel="icon" href="https://newsite.timazi.org/wp-content/uploads/2024/08/cropped-Timazi-Favicon-32x32.png" sizes="32x32">
+   <!-- font awsome lik -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+</head>
+<body>
+
+
+<div class="magazine-hero">
+<div class="magazine-text">
+  <h1 class="display-1">Timazi Magazine</h1>
+  <p class="fs-4">Timazi Magazine exists to TRAIN the Christian High School Student to think and act from a biblical<br> worldview</p>
+  
+</div>
+</div>
+
+
+<div class="container my-5">
+        <h1 class="text-center display-5 sudo position-relative my-5">The Ministry</h1>
+        <div class="row gx-5">
+            <div class="col-sm-12 col-lg-6">
+              <img src="/assets/images/about_timazi-scaled.jpg" class="img-fluid">
+            </div>
+            <div class="col-sm-12 col-lg-6 lh-lg">
+                <p class="pt-5 ">The ministry started with a magazine founded by Gathuku Kibunga together with 
+                    fellow student ministers at Trinity Fellowship in 2007 with an aim to reach High school students
+                     with a Gospel-centred periodical. The periodical was launched as a full color magazine named Timazi
+                      (Swahili word for plumbline) to symbolize  the function of the magazine as a tool to
+                     challenge students to biblical faithfulness.</p>
+            </div>
+        </div>
+    </div>
+
+
+<?php
+include("reuse.html");
+
+?>
+</body>
+</html>
+<?php
+include("footer.html");
+
+?>
---
+++ readership.php
@@ -0,0 +1,53 @@
+<?php
+include("header.html")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timazi Africa</title>
+    <!-- cdn boostrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+    crossorigin="anonymous">
+    <!-- custom css link -->
+    <link rel="stylesheet" href="styles.css" type="text/css">
+    <!-- favicon link -->
+    <link rel="icon" href="https://newsite.timazi.org/wp-content/uploads/2024/08/cropped-Timazi-Favicon-32x32.png" sizes="32x32">
+    <!-- font awsome lik -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="readership-hero">
+<div class="readership-text">
+  <h1 class="display-1  bg text-dark my-5 text-center display-4 sudo position-relative">Readership Challenge</h1>
+  <p class="fs-4">This is a book reading program designed to build a healthy reading culture among high school students.</p>
+  
+</div>
+</div>
+<h1 class="bg text-dark my-5 text-center display-4 sudo position-relative" >Timazi Readership Challenge</h1>
+<div class="container py-3 fs-4" >
+<p>The Timazi Readership Challenge is a book reading program designed to build a healthy 
+reading culture among high school students. The program engages students in different 
+schools that have a pre-paid subscription to read a carefully selected book after which 
+they write a guided report which enter into a national essay writing competition based on 
+the book. Thereafter, a selected team of judges consisting of local Christian leaders and teachers grade the reports.
+ The contest culminates with an award gala on the last weekend of June every year.</p>
+</div>
+
+<div class="container my-4 text-center">
+<iframe width="906" height="510" src="https://www.youtube.com/embed/tKm-tYyQdSQ" title="RC 2024 Sermon:
+     The Pursuit of His Calling.   ||  Dr. Florence Muindi" frameborder="0" allow="accelerometer; autoplay; 
+     clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+    <?php
+    include("reuse.html")
+    ?>
+</body>
+</html>
+<?php
+    include("footer.html")
+    ?>
